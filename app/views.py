66570f91e69cddf6475432298d0eb8056538b131
thezilla <321homerepair@gmail.com>
2014-02-16 12:42:38 -0500

    Added logout

--- app/views.py
+++ app/views.py
@@ -5,9 +5,13 @@ from forms import LoginForm
 from models import User, ROLE_USER, ROLE_ADMIN
 
 
-@app.route('login', methods = ['GET', 'POST'])
+@app.before_request
+def before_request():
+    g.user = current_user
+
+@app.route('/login', methods = ['GET', 'POST'])
 @oid.loginhandler
-def.login():
+def login():
     if g.user is not None and g.user.is_authenticated():
         return redirect(url_for('index'))
     form = LoginForm()
@@ -27,16 +31,18 @@ def after_login(resp):
         return redirect(url_for('login'))
     user = User.query.filter_by(email = resp.email).first()
     if user is None:
-        nickname = resp.email.split('@')[0]
-    user = User{nickname = nickname, email = resp.email, role = ROLE_USER)
-    db.session.add(user)
-    db.session.commit()
-remmeber_me = False
-if 'remember_me' in session:
-    remember_me = session['remember_me']
-    session.pop('remember_me', None)
-login_user(user, remember = remember_me)
-return redirect(request.args.get('next') or url_for('index'))
+        nickname = resp.nickname
+        if nickname is None or nickname == "":
+            nickname = resp.email.split('@')[0]
+        user = User(nickname = nickname, email = resp.email, role = ROLE_USER)
+        db.session.add(user)
+        db.session.commit()
+        remember_me = False
+    if 'remember_me' in session:
+        remember_me = session['remember_me']
+        session.pop('remember_me', None)
+    login_user(user, remember = remember_me)
+    return redirect(request.args.get('next') or url_for('index'))
 
 @app.route('/')
 @app.route('/index')
@@ -71,3 +77,23 @@ def login():
 @lm.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
+@app.route('/user/<nickname>')
+@login_required
+def user(nickname):
+    user = User.query.filter_by(nickname = nickname).first()
+    if user == None:
+        flash('User ' + nickname + ' not found.')
+        return redirect(url_for('index'))
+    posts = [
+        { 'author': user, 'body': 'Test post #1' },
+        { 'author': user, 'body': 'Test post #2' }
+    ]
+    return render_template('user.html',
+        user = user,
+        posts = posts) 
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
