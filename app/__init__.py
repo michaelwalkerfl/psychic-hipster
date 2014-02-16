61ccb6f64048c1641ab73b3bc3bca6aa261c747e
thezilla <321homerepair@gmail.com>
2014-02-16 11:02:58 -0500

    Added login views

--- app/__init__.py
+++ app/__init__.py
@@ -5,5 +5,14 @@ app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
-from app import views, models
+import os
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
 
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
+from app import views, models
